b735ce32522126ce544e269a191fa39c15106310
Tomas Veschambre <tomas.vesch@gmail.com>
2022-07-28 16:59:28 +0200

    BKO-113 Update files with tests working in headless

--- tests/testBKO.spec.ts
+++ tests/testBKO.spec.ts
@@ -10,9 +10,14 @@ test.describe('test Bam Karaoke Online',testBKO);
 test.beforeEach(async ({ page }) => {
     // Log in
     await page.goto(BASE_URL);
-    await page.click('text="Log in"');
+    if(await page.locator('.sc-dacFzL').isVisible()){
+        await page.locator('.sc-dacFzL').click();
+      }
+      if(await page.locator('text="Log in"').isVisible()){
+        await page.click('text="Log in"');
+    }
     await page.locator('[placeholder="Enter your email"]').fill(process.env.AUTH_USER_BKO);
     await page.locator('[placeholder="Enter your password"]').fill(process.env.AUTH_PASS_BKO);
     await page.locator('text=Login').click();
-    await page.waitForSelector('.sc-JAcuL ');
+    await page.waitForTimeout(3000);
 });
